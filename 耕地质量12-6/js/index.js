d3f14595d02bf374e25c92f4ac6de329dcd3bdb2
Is-zhangzhangzhang <Is_zhangjiayi@163.com>
2017-12-06 22:43:35 +0800

    12-6update

---
+++ 耕地质量12-6/js/index.js
@@ -0,0 +1,71 @@
+/*
+* @Author: jiayi
+* @Date:   2017-10-28 21:58:27
+* @Last Modified by:   嘉怡吖
+* @Last Modified time: 2017-11-22 21:13:29
+*/
+
+jQuery(document).ready(function($) {
+
+$(".nav-ul-div").click(function(event) {
+	if($(this).next().is(':visible')== false)
+	{
+		$(".nav-ul ul").slideUp();
+	}
+    
+    $('.nav-ul-div>i').not( $(this).find('.toggle-i') ).addClass('fa-chevron-right').removeClass('fa-chevron-down');
+    $(this).find('.toggle-i').toggleClass('fa-chevron-right fa-chevron-down');
+
+    $(this).next().slideToggle();
+
+    $('.nav-ul-div').removeClass('activecolor');  
+    $(this).addClass('activecolor');
+
+    $('.div-inner-ul li').removeClass('inner-active'); 
+    
+    //设置主界面为none
+    // $('#comment-relate').css('display', 'none');
+});
+
+$(".div-inner-ul li").click(function(event) {
+	$('.div-inner-ul li').removeClass('inner-active');
+	$(this).addClass('inner-active');
+});
+
+/*
+ *关联js
+*/
+ 
+    $(".div-inner-ul li").click(function(event) {
+        var id=$(this).attr("id");
+        var path="page/" + id +".html";
+        setBread(id);
+         $.ajax({
+             url: path,
+             cache: false,
+             success: function(html){
+                 $("#comment-relate").html(html);
+             }
+         });
+        // $.get("../page/relate.html",function(data){  
+        //     $("#comment-relate").html(data);//初始化加载界面  
+        // }); 
+    });
+
+//点击主界面显示
+   
+//面包屑Js  
+ function setBread(id)
+ {
+    $('#getbread').html('');
+    id = "#"+ id;
+    first = $(id).parent().siblings('.nav-ul-div').find('a').text();
+    second = $(id).text();
+    $('#getbread').html('<li>'+
+        '<a href="#">'+first+'</a>'+
+        '<span class="divider"></span></li>'+
+        '<li><a href="#">'+second+'</a>'+
+        '<span class="divider"></span></li>');
+}
+
+});
---
+++ 耕地质量12-6/js/tree.js
@@ -0,0 +1,155 @@
+/*
+* @Author: jiayi
+* @Date:   2017-11-05 14:25:18
+* @Last Modified by:   嘉怡吖
+* @Last Modified time: 2017-11-28 15:50:03
+*/
+jQuery(document).ready(function($) {
+
+    /*
+     * 范围选择js
+     */
+    function addOption(result,id)
+    {
+       // result = eval('('+result+')');
+        result = result.split(':');
+        for(var i=0; i< result.length-1 ; i++)
+        {
+            var sel = $(id);
+            var opt = document.createElement('option');
+            opt.innerHTML = result[i];
+            sel.append(opt);
+        }
+    }
+    
+    //年份选择
+    $.ajax({
+    url: '/FruitTree/LoadYear',
+    type: 'POST',
+    })
+    .done(function(year) {
+        addOption(year,"#select-year");
+    })
+    .fail(function() {
+        console.log("年份获取失败");
+    })
+
+      //点击获取下拉框的值
+    $("#select-year").change(function(event) {
+       //点击的时候type prov dist 3个下拉框全部清空
+       $('.aChoose select:gt(0)').html('<option selected="true" disabled="true" value="0">请选择</option>');
+       //清空树
+       $('#treeView').html("");
+       //取值
+       var text =$(this).find('option:selected').text();
+       getType(text);
+   }); 
+
+
+    //类型选择
+function getType(year)
+{
+    $.ajax({
+        url: '/FruitTree/LoadType',
+        type: 'POST',
+        data: {year : year}
+    })
+    .done(function(type) {
+        $("#select-type").html('<option selected="true" disabled="true" value="0">请选择</option>');
+        addOption(type,"#select-type");
+        $("#select-type").change(function(event) {
+            $('.aChoose select:gt(1)').html('<option selected="true" disabled="true" value="0">请选择</option>');
+            var text =$(this).find('option:selected').text();
+            //获取省/市
+            getProv(year,text);
+         }); 
+    })
+    .fail(function() {
+        console.log("类型获取失败");
+    });
+}
+
+
+    //省/市 选择
+function getProv(year,type)
+{
+     $.ajax({
+        url: '/FruitTree/LoadCity',
+        type: 'POST',
+        data:{
+            year: year,
+            type: type
+        }
+    })
+    .done(function(prov) {
+        //console.log("success");
+        $("#select-prov").html('<option selected="true" disabled="true" value="0">请选择</option>');
+        addOption(prov,"#select-prov");
+        $("#select-prov").change(function (event) {
+            $("#select-dist").html('<option selected="true" disabled="true" value="0">请选择</option>');
+            var text =$(this).find('option:selected').text();
+           getDist(year, type, text);
+         }); 
+    })
+    .fail(function() {
+        console.log("省/市 获取失败");
+    });
+}
+   
+
+    //区/县 选择
+function getDist(year,type,cityname){
+        $.ajax({
+        url: '/FruitTree/LoadCountry',
+        type: 'POST',
+        data: {
+            year: year,
+            type: type,
+            cityname: cityname
+        }
+    })
+    .done(function(dist) {
+        $("#select-dist").html('<option selected="true" disabled="true" value="0">请选择</option>');
+        addOption(dist,"#select-dist")
+        $("#select-dist").change(function (event) {
+             var text =$(this).find('option:selected').text();
+             getTree(year,type,text);
+         }); 
+    })
+    .fail(function() {
+        console.log("区/县 获取失败");
+    });
+}
+
+    /*
+      * tree 的js
+      */
+var setting = {
+    data: {
+        simpleData: {
+            enable: true
+        }
+    }
+};
+function getTree(year,type,countryname)
+{
+     $.ajax({
+         url: '/FruitTree/LoadJson',
+        type: 'POST',
+        //dataType: '',
+        data:{
+            year: year,
+            type: type,
+            countryname:countryname
+        }
+    })
+    .done(function (zNodes) {
+        var znode = eval('(' + zNodes + ')');
+        $.fn.zTree.init($("#treeView"), setting, znode);
+    })
+    .fail(function() {
+        console.log("Tree");
+    });
+}
+
+});
